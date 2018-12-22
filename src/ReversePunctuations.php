8c9b701aec23e7d9278cd59e1ac8b5bc17c034e1
Stanislav2014 <s_admin@mail.ru>
2018-12-22 19:53:06 +0300

    New File

---
+++ src/ReversePunctuations.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanislav
+ * Date: 22.12.18
+ * Time: 17:09
+ */
+
+namespace App\Src;
+
+
+class ReversePunctuations
+{
+    /**
+     * @param $message
+     */
+    public static function reverse ($message)
+    {
+        $regExpPattern1 = '/[^[:punct:]]/';
+
+        $result = array_reverse(preg_split($regExpPattern1, $message, -1, PREG_SPLIT_NO_EMPTY));
+
+        $nresult = '';
+        $words = preg_split('/[[:punct:]]/', $message, -1, PREG_SPLIT_NO_EMPTY);
+
+        foreach ( $words as $key => $word) {
+
+            $nresult .= $word . $result[$key];
+        }
+        return $nresult;
+    }
+
+
+
+}
---
+++ tests/ReversePunctuationsTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanislav
+ * Date: 22.12.18
+ * Time: 19:01
+ */
+
+namespace App\Tests;
+
+use
+class ReversePunctuationsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testReverse()
+    {
+
+    }
+}
